aa3cee891ec1af429f4641173162461346a0e8ba
Miah Raihan Mahmud Arman <raihan2006i@gmail.com>
2015-02-16 13:53:40 +0100

    Redis storage setup in production for Machine learning data storage

--- config/initializers/machine_learning.rb
+++ config/initializers/machine_learning.rb
@@ -1,3 +1,7 @@
 # For the naive bayes implementation
-store = StuffClassifier::RedisStorage.new('classifier')
+if Rails.env.production?
+  store = StuffClassifier::RedisStorage.new('classifier', { url:'redis://rediscloud:BTZLNERLTsfXxG82@pub-redis-19599.eu-west-1-2.1.ec2.garantiadata.com:19599' })
+else
+  store = StuffClassifier::RedisStorage.new('classifier')
+end
 THEMES_BAYES_TRAINER = StuffClassifier::Bayes.new('themes_bayes_trained_data', storage: store)
